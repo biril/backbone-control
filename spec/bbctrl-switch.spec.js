611366ae3c16572bac7357b2a603acfb04cb144a
biril <alexlambiris@gmail.com>
2015-02-17 23:15:17 +0000

    Further split Switch specs into nested suites

--- spec/bbctrl-switch.spec.js
+++ spec/bbctrl-switch.spec.js
@@ -23,200 +23,198 @@ define(['backbone', 'bbctrl-switch'], function (Backbone, Switch) {
 
   describe('The switch control', function () {
 
-    it('should be set to off when initialized without model', function () {
-      var theSwitch = new Switch();
+    describe('in terms of being bound to model state', function () {
 
-      expect(theSwitch.getModelAttrs()).toEqual({ value: false });
-    });
+      it('should be set to off when initialized without model', function () {
+        var theSwitch = new Switch();
 
-    it('should be set to off when initialized with model with false attribute', function () {
-      var theSwitch = new Switch({
-        model: new Backbone.Model({ isOn: false }),
-        modelAttr: 'isOn'
+        expect(theSwitch.getModelAttrs()).toEqual({ value: false });
       });
 
-      expect(theSwitch.getModelAttrs()).toEqual({ isOn: false });
-    });
+      it('should be set to off when initialized with model with false attribute', function () {
+        var theSwitch = new Switch({
+          model: new Backbone.Model({ isOn: false }),
+          modelAttr: 'isOn'
+        });
 
-    it('should be set to on when initialized with model with true attribute', function () {
-      var theSwitch = new Switch({
-        model: new Backbone.Model({ isTurnedOn: true }),
-        modelAttr: 'isTurnedOn'
+        expect(theSwitch.getModelAttrs()).toEqual({ isOn: false });
       });
 
-      expect(theSwitch.getModelAttrs()).toEqual({ isTurnedOn: true });
-    });
+      it('should be set to on when initialized with model with true attribute', function () {
+        var theSwitch = new Switch({
+          model: new Backbone.Model({ isTurnedOn: true }),
+          modelAttr: 'isTurnedOn'
+        });
 
-    ///
-
-    it('should trigger set-request when clicked', function () {
-      var theSwitch, isSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch();
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
+        expect(theSwitch.getModelAttrs()).toEqual({ isTurnedOn: true });
       });
 
-      clickSwitch(theSwitch);
-
-      expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
-    });
-
-    it('should trigger set-to-on-request when set to off and clicked', function () {
-      var theSwitch, isRequestSetToOn;
-      theSwitch = new Switch();
-      theSwitch.on('request:set', function (attrs) {
-        isRequestSetToOn = attrs.value === true;
-      });
-
-      clickSwitch(theSwitch);
-
-      expect(isRequestSetToOn).toBeTruthy();
-    });
-
-    it('should trigger set-to-off-request when set to on and clicked', function () {
-      var theSwitch, isRequestSetToOff;
-      theSwitch = new Switch({
-        model: new Backbone.Model({ isTurnedOn: true }),
-        modelAttr: 'isTurnedOn'
-      });
-      theSwitch.on('request:set', function (attrs) {
-        isRequestSetToOff = attrs.isTurnedOn === false;
-      });
-
-      clickSwitch(theSwitch);
-
-      expect(isRequestSetToOff).toBeTruthy();
-    });
-
-    it('should not cause other instances to trigger set-request when clicked', function () {
-      var theSwitch, otherSwitch, isOtherSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch();
-      otherSwitch = new Switch();
-      otherSwitch.on('request:set', function () {
-        isOtherSwitchSetRequestHandlerInvoked = true;
-      });
-
-      clickSwitch(theSwitch);
-
-      expect(isOtherSwitchSetRequestHandlerInvoked).toBeFalsy();
     });
 
     ///
 
-    it('should not modify model\'s attribute when clicked and is not auto-write-to-model', function () {
-      var theSwitch, switchModel;
-      switchModel = new Backbone.Model({ isOn: false });
-      theSwitch = new Switch({
-        model: switchModel,
-        modelAttr: 'isOn'
+    describe('in terms of setting model state', function () {
+
+      it('should trigger set-request when clicked', function () {
+        var theSwitch, isSwitchSetRequestHandlerInvoked;
+        theSwitch = new Switch();
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
+
+        clickSwitch(theSwitch);
+
+        expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
       });
 
-      clickSwitch(theSwitch);
-      expect(switchModel.get('isOn')).toBeFalsy();
-      clickSwitch(theSwitch);
-      expect(switchModel.get('isOn')).toBeFalsy();
-    });
+      it('should trigger set-to-on-request when set to off and clicked', function () {
+        var theSwitch, isRequestSetToOn;
+        theSwitch = new Switch();
+        theSwitch.on('request:set', function (attrs) {
+          isRequestSetToOn = attrs.value === true;
+        });
 
-    it('should negate model\'s attribute when clicked and is auto-write-to-model ', function () {
-      var theSwitch, switchModel;
-      switchModel = new Backbone.Model({ isOn: false });
-      theSwitch = new Switch({
-        model: switchModel,
-        modelAttr: 'isOn',
-        isAutoWriteToModel: true
+        clickSwitch(theSwitch);
+
+        expect(isRequestSetToOn).toBeTruthy();
       });
 
-      expect(switchModel.get('isOn')).toBeFalsy();
-      clickSwitch(theSwitch);
-      expect(switchModel.get('isOn')).toBeTruthy();
-      clickSwitch(theSwitch);
-      expect(switchModel.get('isOn')).toBeFalsy();
+      it('should trigger set-to-off-request when set to on and clicked', function () {
+        var theSwitch, isRequestSetToOff;
+        theSwitch = new Switch({
+          model: new Backbone.Model({ isTurnedOn: true }),
+          modelAttr: 'isTurnedOn'
+        });
+        theSwitch.on('request:set', function (attrs) {
+          isRequestSetToOff = attrs.isTurnedOn === false;
+        });
+
+        clickSwitch(theSwitch);
+
+        expect(isRequestSetToOff).toBeTruthy();
+      });
+
+      ///
+
+      it('should not modify model\'s attribute when clicked and is not auto-write-to-model', function () {
+        var theSwitch, switchModel;
+        switchModel = new Backbone.Model({ isOn: false });
+        theSwitch = new Switch({
+          model: switchModel,
+          modelAttr: 'isOn'
+        });
+
+        clickSwitch(theSwitch);
+        expect(switchModel.get('isOn')).toBeFalsy();
+        clickSwitch(theSwitch);
+        expect(switchModel.get('isOn')).toBeFalsy();
+      });
+
+      it('should negate model\'s attribute when clicked and is auto-write-to-model', function () {
+        var theSwitch, switchModel;
+        switchModel = new Backbone.Model({ isOn: false });
+        theSwitch = new Switch({
+          model: switchModel,
+          modelAttr: 'isOn',
+          isAutoWriteToModel: true
+        });
+
+        expect(switchModel.get('isOn')).toBeFalsy();
+        clickSwitch(theSwitch);
+        expect(switchModel.get('isOn')).toBeTruthy();
+        clickSwitch(theSwitch);
+        expect(switchModel.get('isOn')).toBeFalsy();
+      });
+
     });
 
     ///
+    describe('in terms of disabled behaviour', function () {
 
-    it('should not trigger set-request when disabled (.disable())', function () {
-      var theSwitch, isSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch();
-      theSwitch.disable();
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
+      it('should not trigger set-request when disabled (.disable())', function () {
+        var theSwitch, isSwitchSetRequestHandlerInvoked;
+        theSwitch = new Switch();
+        theSwitch.disable();
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
+
+        clickSwitch(theSwitch);
+
+        expect(isSwitchSetRequestHandlerInvoked).toBeFalsy();
       });
 
-      clickSwitch(theSwitch);
+      it('should not trigger set-request when disabled (.enable(false))', function () {
+        var theSwitch, isSwitchSetRequestHandlerInvoked;
+        theSwitch = new Switch();
+        theSwitch.enable(false);
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
 
-      expect(isSwitchSetRequestHandlerInvoked).toBeFalsy();
-    });
+        clickSwitch(theSwitch);
 
-    it('should not trigger set-request when disabled (.enable(false))', function () {
-      var theSwitch, isSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch();
-      theSwitch.enable(false);
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
+        expect(isSwitchSetRequestHandlerInvoked).toBeFalsy();
       });
 
-      clickSwitch(theSwitch);
+      it('should not trigger set-request when disabled (uiState.set({ isDisabled: true }))', function () {
+        var theSwitch, isSwitchSetRequestHandlerInvoked;
+        theSwitch = new Switch({
+          uiState: new Backbone.Model({ isDisabled: true })
+        });
+        theSwitch.enable(false);
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
 
-      expect(isSwitchSetRequestHandlerInvoked).toBeFalsy();
-    });
+        clickSwitch(theSwitch);
 
-    it('should not trigger set-request when disabled (uiState.set({ isDisabled: true }))', function () {
-      var theSwitch, isSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch({
-        uiState: new Backbone.Model({ isDisabled: true })
-      });
-      theSwitch.enable(false);
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
-      });
-
-      clickSwitch(theSwitch);
-
-      expect(isSwitchSetRequestHandlerInvoked).toBeFalsy();
-    });
-
-    it('should trigger set-request when clicked after being re-enabled (.enable())', function () {
-      var theSwitch, isSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch();
-      theSwitch.disable();
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
+        expect(isSwitchSetRequestHandlerInvoked).toBeFalsy();
       });
 
-      theSwitch.enable();
-      clickSwitch(theSwitch);
+      it('should trigger set-request when clicked after being re-enabled (.enable())', function () {
+        var theSwitch, isSwitchSetRequestHandlerInvoked;
+        theSwitch = new Switch();
+        theSwitch.disable();
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
 
-      expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
-    });
+        theSwitch.enable();
+        clickSwitch(theSwitch);
 
-    it('should trigger set-request when clicked after being re-enabled (.disable(false))', function () {
-      var theSwitch, isSwitchSetRequestHandlerInvoked;
-      theSwitch = new Switch();
-      theSwitch.disable();
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
+        expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
       });
 
-      theSwitch.disable(false);
-      clickSwitch(theSwitch);
+      it('should trigger set-request when clicked after being re-enabled (.disable(false))', function () {
+        var theSwitch, isSwitchSetRequestHandlerInvoked;
+        theSwitch = new Switch();
+        theSwitch.disable();
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
 
-      expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
-    });
+        theSwitch.disable(false);
+        clickSwitch(theSwitch);
 
-    it('should trigger set-request when clicked after being re-enabled (uiState.set({ isDisabled: false }))', function () {
-      var theSwitch, uiState, isSwitchSetRequestHandlerInvoked;
-      uiState = new Backbone.Model({ isDisabled: true });
-      theSwitch = new Switch({ uiState: uiState });
-      theSwitch.disable();
-      theSwitch.on('request:set', function () {
-        isSwitchSetRequestHandlerInvoked = true;
+        expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
       });
 
-      uiState.set({ isDisabled: false });
-      clickSwitch(theSwitch);
+      it('should trigger set-request when clicked after being re-enabled (uiState.set({ isDisabled: false }))', function () {
+        var theSwitch, uiState, isSwitchSetRequestHandlerInvoked;
+        uiState = new Backbone.Model({ isDisabled: true });
+        theSwitch = new Switch({ uiState: uiState });
+        theSwitch.disable();
+        theSwitch.on('request:set', function () {
+          isSwitchSetRequestHandlerInvoked = true;
+        });
 
-      expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
+        uiState.set({ isDisabled: false });
+        clickSwitch(theSwitch);
+
+        expect(isSwitchSetRequestHandlerInvoked).toBeTruthy();
+      });
+
     });
 
   });
