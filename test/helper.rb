5c53a0e19fa6407ba4d19a023d0d90285bad9966
biril <alexlambiris@gmail.com>
2015-02-26 23:49:52 +0000

    Add more platforms to sauce's 'browsers' config

--- test/helper.rb
+++ test/helper.rb
@@ -14,6 +14,13 @@ Capybara.default_wait_time = 10
 Sauce.config do |c|
   c[:start_tunner] = ENV['RUN_ON_SAUCE']
   c[:browsers] = [
-    ["OS X 10.10", "Chrome", "39"] # TBD
+
+    ['OS X 10.10', 'Chrome',  '40'],
+    ['OS X 10.10', 'Firefox', '35.0'],
+    ['OS X 10.10', 'Safari',  '8.0'],
+
+    ['Windows 7',  'Chrome',  '40'],
+    ['Windows 7',  'Firefox', '35.0'],
+    ['Windows 7',  'Safari',  '5.1']
   ]
 end
